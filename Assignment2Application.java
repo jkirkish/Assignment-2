8a562e1205cdbd313ce7044728cfbfc0f1c508c1
Joseph Kirkish <kirkish.2@gmail.com>
2022-03-28 13:34:44 -0400

    Assignment 2 number guessing game
    java code for a number guessing game

---
+++ Assignment2Application.java
@@ -0,0 +1,17 @@
+package com.coderscampus;
+
+import java.util.Random;
+
+public class Assignment2Application {
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+		NumberGuess game = new NumberGuess();
+		game.runGuessingGame();
+
+	}
+
+}
